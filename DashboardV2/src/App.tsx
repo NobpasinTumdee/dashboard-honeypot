574085ed275b420bf85f19bbac2160341c2fe837
NobpasinTumdee <porporpor579@gmail.com>
2025-06-12 16:04:24 +0700

    sidebar

--- DashboardV2/src/App.tsx
+++ DashboardV2/src/App.tsx
@@ -1,50 +1,71 @@
 import { useState } from 'react'
 import './App.css'
+import './Styles/Sidebar.css'
+import Logo from './assets/Honeypot-logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   //theme web
   const setDarkMode = () => {
-    document.querySelector("body")?.setAttribute("data-theme","dark");
-    localStorage.setItem("SelectedTheme","dark")
+    document.querySelector("body")?.setAttribute("data-theme", "dark");
+    localStorage.setItem("SelectedTheme", "dark")
   }
   const setLightMode = () => {
-    document.querySelector("body")?.setAttribute("data-theme","light");
-    localStorage.setItem("SelectedTheme","light")
+    document.querySelector("body")?.setAttribute("data-theme", "light");
+    localStorage.setItem("SelectedTheme", "light")
   }
   const SelectedTheme = localStorage.getItem("SelectedTheme");
   if (SelectedTheme === "dark") {
     setDarkMode();
   }
-  const toggleTheme = (e : any) => {
+  const toggleTheme = (e: any) => {
     if (e.target.checked) setDarkMode();
     else setLightMode();
   }
 
+
+  //side bar
+  const [isSidebar, setSidebar] = useState(true)
+
   return (
     <>
-      <input
-        // checkbox for theme web
-        type="checkbox"
-        id='darkmode-toggle'
-        onChange={toggleTheme}
-        defaultChecked={SelectedTheme === "dark"}
-      />
-
-
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      {/* <img src={Logo} alt="logo" width={70} onClick={() => setSidebar(!isSidebar)} /> */}
+      <div className='main'>
+        <div className={`sidebar ${isSidebar ? 'open' : 'closed'}`}>
+          <img src={Logo} alt="logo" width={70} onClick={() => setSidebar(!isSidebar)} />
+          <p>PAGES</p>
+          <ul>
+            <li className='side-menu'>🏠 Overview</li>
+            <li className='side-menu'>🐚 Cowrie</li>
+            <li className='side-menu'>🦖 Dionaea</li>
+            <li className='side-menu'>🦈 Wire Shark</li>
+          </ul>
+          <p>Chat bot AI</p>
+          <ul>
+            <li className='side-menu'>Chat GPT</li>
+            <li className='side-menu'>Gemini</li>
+            <li className='side-menu'>Ollama</li>
+            <li className='side-menu'>Foundry</li>
+            <li className='side-menu'>PorGz</li>
+          </ul>
+        </div>
+        <div className='main-dashbord'>
+          <div className='header'>
+            <input
+              // checkbox for theme web
+              type="checkbox"
+              id='darkmode-toggle'
+              onChange={toggleTheme}
+              defaultChecked={SelectedTheme === "dark"}
+            />
+            header
+          </div>
+          <div>
+            dashbord
+          </div>
+        </div>
       </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
