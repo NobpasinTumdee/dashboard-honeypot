3c80c776b7e4c80ef92239b769714075a21af87f
NobpasinTumdee <porporpor579@gmail.com>
2025-06-12 22:35:49 +0700

    header and main mock up

--- DashboardV2/src/App.tsx
+++ DashboardV2/src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import './Styles/Sidebar.css'
+import './Styles/Header.css'
+import './Styles/Dashborad.css'
 import Logo from './assets/Honeypot-logo.png'
+import { BarChart, LineChart } from '@mui/x-charts'
 
 function App() {
 
@@ -29,10 +32,20 @@ function App() {
 
   return (
     <>
-      {/* <img src={Logo} alt="logo" width={70} onClick={() => setSidebar(!isSidebar)} /> */}
+      <div className="hover-wrapper">
+        <img
+          className="toggle-button"
+          src={Logo}
+          alt="logo"
+          width={70}
+          onClick={() => setSidebar(!isSidebar)}
+        />
+        <span className="hover-text">Open Sidebar</span>
+      </div>
+
       <div className='main'>
         <div className={`sidebar ${isSidebar ? 'open' : 'closed'}`}>
-          <img src={Logo} alt="logo" width={70} onClick={() => setSidebar(!isSidebar)} />
+          <img src={Logo} alt="logo" width={70} onClick={() => setSidebar(!isSidebar)} style={{ cursor: 'pointer' }} />
           <p>PAGES</p>
           <ul>
             <li className='side-menu'>🏠 Overview</li>
@@ -51,17 +64,96 @@ function App() {
         </div>
         <div className='main-dashbord'>
           <div className='header'>
-            <input
-              // checkbox for theme web
-              type="checkbox"
-              id='darkmode-toggle'
-              onChange={toggleTheme}
-              defaultChecked={SelectedTheme === "dark"}
-            />
-            header
+            <label className="switch-with-icon">
+              <input
+                // checkbox for theme web
+                type="checkbox"
+                id='darkmode-toggle'
+                onChange={toggleTheme}
+                defaultChecked={SelectedTheme === "dark"}
+              />
+              <span className="slider">
+                <span className="icon sun">☀️</span>
+                <span className="icon moon">🌙</span>
+              </span>
+            </label>
+            <img className='Profile-Admin' src="https://i.pravatar.cc/40" alt="Profile" />
+            <p>PorGz</p>
           </div>
-          <div>
-            dashbord
+          <div className='dashborad-main'>
+            <h2>Dashborad</h2>
+            <div className='overview-in-main'>
+              <div className='chart-mini-size'>
+                <p>Cowrie</p>
+                <LineChart
+                  sx={{
+                    backdropFilter: 'blur(10px)',
+                    borderRadius: 2,
+                    border: '1px solid var(--line_header)',
+                    margin: '10px'
+                  }}
+                  xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                  series={[
+                    {
+                      data: [2, 5.5, 2, 8.5, 1.5, 5],
+                    },
+                  ]}
+                  height={200}
+                />
+              </div>
+              <div className='chart-mini-size'>
+                <p>Dionaea</p>
+                <LineChart
+                  sx={{
+                    backdropFilter: 'blur(10px)',
+                    borderRadius: 2,
+                    border: '1px solid var(--line_header)',
+                    margin: '10px'
+                  }}
+                  xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                  series={[
+                    {
+                      data: [2, 2, 6, 8.5, 1.5, 5],
+                    },
+                  ]}
+                  height={200}
+                />
+              </div>
+              <div className='chart-mini-size'>
+                <p>Wire Shark</p>
+                <LineChart
+                  sx={{
+                    backdropFilter: 'blur(10px)',
+                    borderRadius: 2,
+                    border: '1px solid var(--line_header)',
+                    margin: '10px'
+                  }}
+                  xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                  series={[
+                    {
+                      data: [2, 5, 1, 8.5, 15, 5],
+                    },
+                  ]}
+                  height={200}
+                />
+              </div>
+            </div>
+            <div className='overview-in-main'>
+              <div className='chart-mini-size'>
+                <p>Overview</p>
+                <BarChart
+                  xAxis={[{ data: ['group A', 'group B', 'group C'] }]}
+                  series={[{ data: [4, 3, 5] ,color: '#8470FF'}, { data: [1, 6, 3] ,color: '#67BFFF'}, { data: [2, 5, 6] ,color: '#61C8B5'}]}
+                />
+              </div>
+              <div className='chart-mini-size'>
+                <p>I don't Know bro.</p>
+                <BarChart
+                  xAxis={[{ data: ['group A', 'group B', 'group C'] }]}
+                  series={[{ data: [4, 3, 5] ,color: '#8470FF'}, { data: [1, 6, 3] ,color: '#67BFFF'}, { data: [2, 5, 6] ,color: '#61C8B5'}]}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
