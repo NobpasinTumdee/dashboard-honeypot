b966de5f24a9e60e1290899394d94b89b60b1291
NobpasinTumdee <porporpor579@gmail.com>
2025-08-08 15:12:39 +0700

    chart cowrie --> websocket

--- DashboardV2/src/components/Home.tsx
+++ DashboardV2/src/components/Home.tsx
@@ -5,17 +5,16 @@ import 'aos/dist/aos.css';
 import { useEffect, useState } from 'react';
 import ChartComponent from './chart/Chart';
 import type { AlertItem } from './Cowrie';
-import { getCowrie, getCowrieAuth } from '../serviceApi';
+import { getCowrie } from '../serviceApi';
 import ChartByDateComponent from './chart/ChartByDateComponent';
+import { useCowrieSocket } from './web-socket/controller';
 
 
-const isLogin = localStorage.getItem("isLogin");
 
 const Home = () => {
     useEffect(() => {
         (async () => {
             await handleFetchData();
-            await handleFetchDataAuth();
         })();
         Aos.init({
             duration: 1000,
@@ -49,38 +48,24 @@ const Home = () => {
             setData([])
         }
     };
-    const [dataAuth, setDataAuth] = useState<AlertItem[]>([]);
 
-    const handleFetchDataAuth = async () => {
-        try {
-            const res = await getCowrieAuth();
-            const responseData = res?.data;
-            if (!responseData) {
-                console.error("No data received from API");
-                setDataAuth([]);
-                return;
-            }
-            if (Array.isArray(responseData)) {
-                setDataAuth(responseData);
-            } else if (Array.isArray(responseData?.data)) {
-                setDataAuth(responseData.data);
-            } else {
-                console.error("Unexpected response:", responseData);
-                setDataAuth([]);
-            }
-        } catch (error) {
-            setDataAuth([])
-        }
-    };
+
+    const [dataAuth, setDataAuth] = useState<AlertItem[]>([]);
+    const [isConnected, setIsConnected] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+
+    // Custom hook to manage WebSocket connection
+    useCowrieSocket(setDataAuth, setIsConnected, setIsLogin);
     // ============================================
     return (
         <>
             <div className='main'>
                 <div className='dashborad-main'>
                     <h1 style={{ textAlign: 'center' }} data-aos="zoom-in-down">Dashborad</h1>
-                    {isLogin === "true" ? (
+                    {isLogin ? (
                         <>
                             <h2 style={{ textAlign: 'center' }} data-aos="zoom-in-down">Cowrie</h2>
+                            <p data-aos="zoom-in-down" style={{ textAlign: 'center' }}>data length: {dataAuth.length} status: {isConnected ? '🟢 Connected' : '🔴 Disconnected'}</p>
                             <div className='chart-in-main'>
                                 <div className='chart-in-sub'>
                                     <ChartComponent logs={dataAuth} />
@@ -102,7 +87,8 @@ const Home = () => {
                     ) : (
                         <>
                             <h2 style={{ textAlign: 'center' }} data-aos="zoom-in-down">Cowrie</h2>
-                            <p style={{ textAlign: 'center' , color: 'red'}} data-aos="zoom-in-down">Please log in to get full information.</p>
+                            <p style={{ textAlign: 'center', color: 'red' }} data-aos="zoom-in-down">Please log in to get full information.</p>
+                            <p>data length: {data.length}</p>
                             <div className='chart-in-main'>
                                 <div className='chart-in-sub'>
                                     <ChartComponent logs={data} />
