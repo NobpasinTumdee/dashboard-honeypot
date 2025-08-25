a62424e4b4aef726391f4326cf4f7db30402494c
NobpasinTumdee <porporpor579@gmail.com>
2025-08-26 01:17:34 +0700

    อัพเดทui ทุกหน้าเพื่อเพิ่มรายระเอียดในการมอนิเตอร์

--- DashboardV2/src/components/Navbar.tsx
+++ DashboardV2/src/components/Navbar.tsx
@@ -58,40 +58,40 @@ const Navbar = () => {
                             </>
                         )}
                         <Link to="document" className={`Link-button ${isActive('/document') ? 'active' : ''}`}> Document </Link>
-                        <div className='group-menu'>
-                            {isLogin === "true" && (
-                                <>
-                                    <Link to="chatbot" className="icon-nav">💬</Link>
-                                </>
-                            )}
-                            <div className="theme-toggle-wrapper">
-                                <label className="toggle-switch">
-                                    {/* // checkbox for theme web */}
-                                    <input type="checkbox" id='darkmode-toggle' onChange={toggleTheme} defaultChecked={SelectedTheme === "dark"} />
-                                    <span className="slider">
-                                        <div className="clouds">
-                                            <svg viewBox="0 0 100 100" className="cloud cloud1">
-                                                <path
-                                                    d="M30,45 Q35,25 50,25 Q65,25 70,45 Q80,45 85,50 Q90,55 85,60 Q80,65 75,60 Q65,60 60,65 Q55,70 50,65 Q45,70 40,65 Q35,60 25,60 Q20,65 15,60 Q10,55 15,50 Q20,45 30,45"
-                                                ></path>
-                                            </svg>
-                                            <svg viewBox="0 0 100 100" className="cloud cloud2">
-                                                <path
-                                                    d="M30,45 Q35,25 50,25 Q65,25 70,45 Q80,45 85,50 Q90,55 85,60 Q80,65 75,60 Q65,60 60,65 Q55,70 50,65 Q45,70 40,65 Q35,60 25,60 Q20,65 15,60 Q10,55 15,50 Q20,45 30,45"
-                                                ></path>
-                                            </svg>
-                                        </div>
-                                    </span>
-                                </label>
-                            </div>
-                            {isLogin === "true" &&
+                        {isLogin === "true" && (
+                            <Link to="chatbot" className="icon-nav">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--body_text_color)"><path d="M180-380q-41.92 0-70.96-29.04Q80-438.08 80-480q0-41.92 29.04-70.96Q138.08-580 180-580v-87.69q0-29.83 21.24-51.07Q222.48-740 252.31-740H380q0-41.92 29.04-70.96Q438.08-840 480-840q41.92 0 70.96 29.04Q580-781.92 580-740h127.69q29.83 0 51.07 21.24Q780-697.52 780-667.69V-580q41.92 0 70.96 29.04Q880-521.92 880-480q0 41.92-29.04 70.96Q821.92-380 780-380v167.69q0 29.83-21.24 51.07Q737.52-140 707.69-140H252.31q-29.83 0-51.07-21.24Q180-182.48 180-212.31V-380Zm179.95-70q20.82 0 35.43-14.57Q410-479.14 410-499.95q0-20.82-14.57-35.43Q380.86-550 360.05-550q-20.82 0-35.43 14.57Q310-520.86 310-500.05q0 20.82 14.57 35.43Q339.14-450 359.95-450Zm240 0q20.82 0 35.43-14.57Q650-479.14 650-499.95q0-20.82-14.57-35.43Q620.86-550 600.05-550q-20.82 0-35.43 14.57Q550-520.86 550-500.05q0 20.82 14.57 35.43Q579.14-450 599.95-450ZM330-290h300v-60H330v60Zm-77.69 90h455.38q5.39 0 8.85-3.46t3.46-8.85v-455.38q0-5.39-3.46-8.85t-8.85-3.46H252.31q-5.39 0-8.85 3.46t-3.46 8.85v455.38q0 5.39 3.46 8.85t8.85 3.46ZM480-440Z" /></svg>
+                            </Link>
+                        )}
+                        <div className="theme-toggle-wrapper">
+                            <label className="toggle-switch">
+                                {/* // checkbox for theme web */}
+                                <input type="checkbox" id='darkmode-toggle' onChange={toggleTheme} defaultChecked={SelectedTheme === "dark"} />
+                                <span className="slider">
+                                    <div className="clouds">
+                                        <svg viewBox="0 0 100 100" className="cloud cloud1">
+                                            <path
+                                                d="M30,45 Q35,25 50,25 Q65,25 70,45 Q80,45 85,50 Q90,55 85,60 Q80,65 75,60 Q65,60 60,65 Q55,70 50,65 Q45,70 40,65 Q35,60 25,60 Q20,65 15,60 Q10,55 15,50 Q20,45 30,45"
+                                            ></path>
+                                        </svg>
+                                        <svg viewBox="0 0 100 100" className="cloud cloud2">
+                                            <path
+                                                d="M30,45 Q35,25 50,25 Q65,25 70,45 Q80,45 85,50 Q90,55 85,60 Q80,65 75,60 Q65,60 60,65 Q55,70 50,65 Q45,70 40,65 Q35,60 25,60 Q20,65 15,60 Q10,55 15,50 Q20,45 30,45"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </span>
+                            </label>
+                        </div>
+                        {isLogin === "true" &&
+                            <>
                                 <Link to={'login'} style={{ display: 'flex' }}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="var(--body_text_color)" style={{ opacity:'0.8'}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 -960 960 960" width="25px" fill="var(--body_text_color)" style={{ opacity: '0.8' }}>
                                         <path d="M202.87-111.87q-37.78 0-64.39-26.61t-26.61-64.39v-554.26q0-37.78 26.61-64.39t64.39-26.61h279.04v91H202.87v554.26h279.04v91H202.87Zm434.02-156.65L574-333.93 674.56-434.5H358.09v-91h316.47L574-626.07l62.89-65.41L848.13-480 636.89-268.52Z" />
                                     </svg>
                                 </Link>
-                            }
-                        </div>
+                            </>
+                        }
                         {/* <img className="Profile" src="https://i.pravatar.cc/40" alt="Profile" /> */}
                     </div>
                 </div>
--- DashboardV2/src/layouts/Rootlayout.tsx
+++ DashboardV2/src/layouts/Rootlayout.tsx
@@ -2,25 +2,44 @@ import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import '../Styles/Navbar.css'
 import LightRays from '../components/documentation/components/LightRays'
+import { useEffect, useState } from 'react'
 
 const Rootlayout = () => {
+  const [background, setBackground] = useState(() => {
+    const savedBackground = localStorage.getItem("background");
+    return savedBackground === "true" ? true : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("background", background.toString());
+  }, [background]);
+
+  const toggleBackground = () => {
+    setBackground(prevBackground => !prevBackground);
+  };
+
   return (
     <>
       <Navbar />
       <div className='layout-overlap' />
-      <div style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: -1, top: 0 }}>
-        <LightRays
-          raysOrigin="top-center"
-          raysColor="#DFD7AF"
-          raysSpeed={5}
-          lightSpread={0.8}
-          rayLength={2}
-          followMouse={false}
-          mouseInfluence={0.1}
-          noiseAmount={0.1}
-          distortion={5}
-          className="custom-rays"
-        />
+      {!background &&
+        <div style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: -1, top: 0 }}>
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="#DFD7AF"
+            raysSpeed={5}
+            lightSpread={0.8}
+            rayLength={2}
+            followMouse={false}
+            mouseInfluence={0.1}
+            noiseAmount={0.1}
+            distortion={5}
+            className="custom-rays"
+          />
+        </div>
+      }
+      <div style={{ position: 'fixed', zIndex: 1, bottom: '10px', right: '10px', cursor: 'pointer' }} onClick={toggleBackground}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--body_text_color)"><path d="M680-640q-17 0-28.5-11.5T640-680q0-17 11.5-28.5T680-720q17 0 28.5 11.5T720-680q0 17-11.5 28.5T680-640ZM360-400l108-140 62 80 92-120 138 180H360ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm80-505v-215q0-33 23.5-56.5T320-880h200v80H320v215h-80Zm80 345q-33 0-56.5-23.5T240-320v-185h80v185h200v80H320Zm280 0v-80h200v-185h80v185q0 33-23.5 56.5T800-240H600Zm200-345v-215H600v-80h200q33 0 56.5 23.5T880-800v215h-80Z" /></svg>
       </div>
       <Outlet />
     </>
