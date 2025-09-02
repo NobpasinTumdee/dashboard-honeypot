d916ba040ef0070afb7e333f6d543f0496a9eac4
NobpasinTumdee <porporpor579@gmail.com>
2025-09-02 16:43:34 +0700

    เงื่อนไขการ login (มีคำอธิบาย)
    ถ้า username ตัวแรกเป็น a A r R จะไปหน้า admin
    แต่ถ้า user  เป็นอะไรไม่รู้จะไปหน้า ผู้ใช้

--- Plugin/Opencannary/skin/Admin/login.js
+++ Plugin/Opencannary/skin/Admin/login.js
@@ -10,8 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ทำเหมือนตรวจสอบจริง
     if (username.length > 0 && password.length > 0) {
-      // เข้าสู่ dashboard เสมอ
-      window.location.href = "dashboard.html";
+      const firstChar = username.charAt(0).toLowerCase(); // เอาตัวแรกแล้วทำเป็น lowercase
+      if (["a", "r"].includes(firstChar)) {
+        window.location.href = "dashboard.html";
+      }else{
+        window.location.href = "user/hero.html";
+      }
     } else {
       errorMsg.textContent = "Invalid credentials, please try again.";
     }
