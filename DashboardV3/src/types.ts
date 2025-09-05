0b75f49df303cdaf7bf29bf435cbfa5dfc677e40
NobpasinTumdee <porporpor579@gmail.com>
2025-09-05 21:11:02 +0700

    add service api and websocket

--- DashboardV3/src/types.ts
+++ DashboardV3/src/types.ts
@@ -61,10 +61,30 @@ export interface HttpsPacket {
     userAgent: string;
 }
 
-// Navigation Types
-export interface NavItem {
-    id: string;
-    label: string;
-    path: string;
-    icon: string;
+// Wireshark filter Types
+export interface TimeSeriesPackets {
+  id: number;
+  timestamp: string;
+  count: number;
+}
+
+export interface ProtocolStats {
+    id: number;
+    protocol: string;
+    timestamp: string;
+    count: number;
+}
+
+export interface SrcIpStats {
+    id: number;
+    src_ip: string;
+    timestamp: string;
+    count: number;
+}
+
+export interface DstPortStats {
+    id: number;
+    dst_port: string;
+    timestamp: string;
+    count: number;
 }
